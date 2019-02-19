ad01c96f4853157cc41ea8b09c48483f91d9740c
Yuan Qing <desk@yuanqing.sg>
2019-02-19 23:50:07 +0800

    Bikeshed

--- src/api/appcast/read-appcast-versions.js
+++ src/api/appcast/read-appcast-versions.js
@@ -5,10 +5,9 @@ async function readAppcastVersions (appcastPath) {
   const xml = await fs.readFile(appcastPath, 'utf8')
   const appcast = JSON.parse(xmlJs.xml2json(xml, { compact: true, spaces: 2 }))
   const items = [].concat(appcast.rss.channel.item)
-  const versions = items.map(function (item) {
+  return items.map(function (item) {
     return item.title._text
   })
-  return versions
 }
 
 module.exports = readAppcastVersions
