e0064528898aba714fb7bc88a50e2bcca86771f1
yuanqing <desk@yuanqing.sg>
2019-02-28 01:35:05 +0800

    Support multi-field form

--- src/settings/open-settings-dialog.js
+++ src/settings/open-settings-dialog.js
@@ -1,12 +1,24 @@
 const readSettings = require('./read-settings')
 
-const width = 200
+const width = 300
 const rowHeight = 20
+
+function createLabel (value, y) {
+  const label = NSTextField.alloc().initWithFrame(
+    NSMakeRect(0, y, width, rowHeight)
+  )
+  label.setStringValue(value)
+  label.setSelectable(false)
+  label.setEditable(false)
+  label.setBezeled(false)
+  label.setDrawsBackground(false)
+  return label
+}
 
 const createForm = {
   textBox: function (value, y) {
     const form = NSTextField.alloc().initWithFrame(
-      NSMakeRect(0, 0, width, rowHeight)
+      NSMakeRect(0, y, width, rowHeight)
     )
     form.setStringValue(value)
     return form
@@ -18,22 +30,26 @@ function openSettingsDialog (title, fields) {
   alert.setMessageText(title)
   alert.addButtonWithTitle('OK')
   alert.addButtonWithTitle('Cancel')
+  const fieldHeight = rowHeight + rowHeight
   const view = NSView.alloc().initWithFrame(
-    NSMakeRect(0, 0, width, fields.length * rowHeight)
+    NSMakeRect(0, 0, width, fields.length * (fieldHeight + rowHeight))
   )
   alert.addAccessoryView(view)
   const settings = readSettings()
-  const forms = fields.map(function ({ type, key }, index) {
-    const form = createForm[type](settings[key], index * rowHeight)
-    view.addSubview(form)
+  const forms = fields.map(function ({ key, name, type }, index) {
+    const y = index * (fieldHeight + rowHeight)
+    const formLabel = createLabel(name, y + rowHeight)
+    view.addSubview(formLabel)
+    const formField = createForm[type](settings[key], y)
+    view.addSubview(formField)
     return {
       key,
-      form
+      formField
     }
   })
   if (alert.runModal() === '1000') {
-    return forms.reduce(function (result, { key, form }) {
-      result[key] = retrieveValue(form)
+    return forms.reduce(function (result, { key, formField }) {
+      result[key] = retrieveValue(formField)
       return result
     }, {})
   }
