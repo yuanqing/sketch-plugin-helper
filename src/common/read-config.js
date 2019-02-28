21c4379753227cd595f4dccbfb3e2ad0da551cab
yuanqing <desk@yuanqing.sg>
2019-02-28 23:15:42 +0800

    Require `get-package-json` on demand

--- src/common/read-config.js
+++ src/common/read-config.js
@@ -2,10 +2,10 @@ const path = require('path')
 const fs = require('fs-extra')
 
 const readAppcastVersions = require('./appcast/read-appcast-versions')
-const packageJson = require('./get-package-json')
 const { appcastFileName } = require('./constants')
 
 async function readConfig () {
+  const packageJson = require('./get-package-json')
   const appcastPath = path.join(process.cwd(), appcastFileName)
   const versions = (await fs.exists(appcastPath))
     ? await readAppcastVersions(appcastPath)
