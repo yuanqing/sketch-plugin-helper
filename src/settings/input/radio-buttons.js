9e1bbb3063cc9a4890c42724e0b6871f886aa951
yuanqing <desk@yuanqing.sg>
2019-03-03 23:55:43 +0800

    Complete `radio-buttons`

--- src/settings/input/radio-buttons.js
+++ src/settings/input/radio-buttons.js
@@ -1,22 +1,29 @@
-function radioButtons (values, activeIndex, x, y, width, height) {
+function radioButtons ({ width, height, value, possibleValues }) {
+  console.log(value)
   const buttonFormat = NSButtonCell.alloc().init()
   buttonFormat.setButtonType(NSRadioButton)
-  const length = values.length
-  const matrixFormat = NSMatrix.alloc().initWithFrame_mode_prototype_numberOfRows_numberOfColumns(
-    NSMakeRect(x, y, width, height),
+  const length = possibleValues.length
+  const matrix = NSMatrix.alloc().initWithFrame_mode_prototype_numberOfRows_numberOfColumns(
+    NSMakeRect(0, 0, width, height),
     NSRadioModeMatrix,
     buttonFormat,
     1,
     length
   )
-  matrixFormat.setCellSize(CGSizeMake(width / length, height))
-  const cells = matrixFormat.cells()
-  let i = -1
-  while (++i < length) {
-    cells.objectAtIndex(i).setTitle(values[i])
+  matrix.setCellSize(CGSizeMake(Math.floor(width / length), height))
+  const cells = matrix.cells()
+  cells.forEach(function (cell, index) {
+    cell.setTitle(possibleValues[index])
+  })
+  const index = possibleValues.indexOf(value)
+  matrix.selectCellAtRow_column(0, index)
+  return {
+    view: matrix,
+    retrieveValue: function () {
+      const index = matrix.cells().indexOfObject(matrix.selectedCell())
+      return possibleValues[index]
+    }
   }
-  matrixFormat.selectCellAtRow_column(0, activeIndex)
-  return matrixFormat
 }
 
 module.exports = radioButtons
