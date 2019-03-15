e7cf9cbac3e69105c56da5f06b03a16bdf595679
yuanqing <desk@yuanqing.sg>
2019-03-15 21:37:32 +0800

    Use helper method

--- src/template/src/hello-world.js
+++ src/template/src/hello-world.js
@@ -1,7 +1,6 @@
-const { readSettings } = require('sketch-plugin-helper')
-const UI = require('sketch/ui')
+const { readSettings, showMessage } = require('sketch-plugin-helper')
 
 export default function () {
   const { greeting, name } = readSettings()
-  UI.message(`${greeting}, ${name}`)
+  showMessage(`${greeting}, ${name}`)
 }
