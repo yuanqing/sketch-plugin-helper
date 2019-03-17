dff92db0027cef1db8ef1e20df4667a2b6646d55
yuanqing <desk@yuanqing.sg>
2019-03-17 22:36:20 +0800

    Watch package.json

--- src/common/watch.js
+++ src/common/watch.js
@@ -5,7 +5,7 @@ const { sourceDirectory } = require('./constants')
 
 function watch ({ onReady, onChange, onSuccess }) {
   return new Promise(function (resolve, reject) {
-    const watcher = chokidar.watch(sourceDirectory)
+    const watcher = chokidar.watch([sourceDirectory, './package.json'])
     watcher.on('ready', onReady)
     watcher.on('change', async function () {
       onChange()
