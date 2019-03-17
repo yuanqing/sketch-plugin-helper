fa9d1117d47ee1cdd54d885fc29c3b989a1129e1
yuanqing <desk@yuanqing.sg>
2019-03-17 20:03:03 +0800

    Update template

--- src/template/src/hello-world.js
+++ src/template/src/hello-world.js
@@ -1,6 +1,33 @@
-const { getSavedUserInput, showMessage } = require('sketch-plugin-helper')
+const {
+  openUserInputDialog,
+  saveUserInput,
+  showMessage,
+  TEXT_BOX,
+  DROP_DOWN
+} = require('sketch-plugin-helper')
 
-export default function () {
-  const { greeting, name } = getSavedUserInput()
+function helloWorld () {
+  const userInput = openUserInputDialog({
+    title: 'Settings',
+    inputs: [
+      {
+        key: 'greeting',
+        label: 'Greeting',
+        type: DROP_DOWN,
+        possibleValues: ['Hello', 'Goodbye']
+      },
+      {
+        key: 'name',
+        label: 'Name',
+        type: TEXT_BOX
+      }
+    ]
+  })
+  if (userInput) {
+    saveUserInput(userInput)
+  }
+  const { greeting, name } = userInput
   showMessage(`${greeting}, ${name}`)
 }
+
+module.exports = helloWorld
