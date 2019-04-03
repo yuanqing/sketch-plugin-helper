9dc0f35ce204cf5d7381b2ca8021aadf42e1a6af
yuanqing <desk@yuanqing.sg>
2019-04-03 21:02:10 +0800

    Limit versions choices

--- src/commands/version.js
+++ src/commands/version.js
@@ -5,6 +5,12 @@ import { updateVersion } from '../common/update-version'
 export const version = {
   command: 'version <type>',
   describe: 'Updates the version of the Sketch plugin',
+  builder: function (yargs) {
+    yargs.positional('type', {
+      type: 'string',
+      choices: ['patch', 'minor', 'major']
+    })
+  },
   handler: async function ({ type }) {
     const logger = createLogger()
     logger.loading('Updating version...')
