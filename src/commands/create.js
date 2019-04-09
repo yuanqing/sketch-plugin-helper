089ff52f495013878db97b6ebbe46a0e32790c45
yuanqing <desk@yuanqing.sg>
2019-04-09 22:05:06 +0800

    Make `sph create` an interactive prompt

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,41 +1,55 @@
+import gitUserName from 'git-user-name'
+import { prompt } from 'inquirer'
+
 import { createErrorHandler } from '../common/create-error-handler'
 import { createLogger } from '../common/create-logger'
 import { scaffoldPlugin } from '../common/scaffold-plugin'
 
+const questions = [
+  {
+    type: 'input',
+    name: 'pluginName',
+    message: 'Plugin name'
+  },
+  {
+    type: 'input',
+    name: 'pluginDescription',
+    message: 'Plugin description'
+  },
+  {
+    type: 'input',
+    name: 'authorName',
+    message: 'Author name',
+    default: function () {
+      return gitUserName()
+    }
+  },
+  {
+    type: 'input',
+    name: 'githubUserName',
+    message: 'Github user name',
+    default: function () {
+      return gitUserName()
+    }
+  },
+  {
+    type: 'input',
+    name: 'githubRepositoryName',
+    message: 'Github repository name'
+  }
+]
+
 export const create = {
   command: 'create',
   describe: 'Scaffolds a new Sketch plugin',
-  builder: function (yargs) {
-    yargs.option('pluginName', {
-      alias: ['name', 'n'],
-      demandOption: true
-    })
-    yargs.option('pluginDescription', {
-      alias: ['description', 'd'],
-      demandOption: true
-    })
-    yargs.option('authorName', {
-      alias: ['author', 'a'],
-      demandOption: true
-    })
-    yargs.option('githubUserName', {
-      alias: ['username', 'u'],
-      demandOption: true
-    })
-    yargs.option('githubRepositoryName', {
-      alias: ['repository', 'r'],
-      demandOption: true
-    })
-  },
-  handler: async function (config) {
-    const logger = createLogger()
-    logger.loading('Creating new plugin...')
+  handler: async function () {
+    const config = await prompt(questions)
     const outputDirectoryPath = process.cwd()
+    const logger = createLogger()
     await scaffoldPlugin({
       outputDirectoryPath,
       config
     }).catch(createErrorHandler(logger))
     logger.succeed('Created new plugin')
-    return Promise.resolve()
   }
 }
