a2f55e35e7cf38999ab336c0e2cda38e7208a96b
yuanqing <desk@yuanqing.sg>
2019-04-14 01:42:00 +0800

    Always have nested commands

--- src/common/build-plugin/build-manifest.js
+++ src/common/build-plugin/build-manifest.js
@@ -44,10 +44,6 @@ async function createManifest ({
       commands
     })
   }
-  if (menu.items.length === 1) {
-    menu.isRoot = true
-    delete menu.title
-  }
   return {
     name: pluginName,
     description: pluginDescription,
