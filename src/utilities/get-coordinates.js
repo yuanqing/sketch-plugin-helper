d36b3e784df5ce591e42bcf32d457b54c5f85707
yuanqing <desk@yuanqing.sg>
2019-04-17 22:29:21 +0800

    Add `getCoordinatesRelativeToArtboard`

--- src/utilities/get-coordinates.js
+++ src/utilities/get-coordinates.js
@@ -19,3 +19,22 @@ function getAbsoluteCoordinatesHelper (layer, result) {
   }
   return getAbsoluteCoordinatesHelper(parent, newResult)
 }
+
+export function getCoordinatesRelativeToArtboard (layer) {
+  return getCoordinatesRelativeToArtboardHelper(layer, {
+    x: layer.frame.x,
+    y: layer.frame.y
+  })
+}
+
+function getCoordinatesRelativeToArtboardHelper (layer, result) {
+  const parent = layer.parent
+  if (parent.type === 'Artboard') {
+    return result
+  }
+  const newResult = {
+    x: result.x + parent.frame.x,
+    y: result.y + parent.frame.y
+  }
+  return getCoordinatesRelativeToArtboardHelper(parent, newResult)
+}
