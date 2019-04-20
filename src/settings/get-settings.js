5e5129f22697c72139e4f30f34887fef7047e73a
yuanqing <desk@yuanqing.sg>
2019-04-20 19:11:43 +0800

    Add ability to filter settings by `keyPrefix`

--- src/settings/get-settings.js
+++ src/settings/get-settings.js
@@ -1,17 +1,46 @@
 import { sessionVariable, settingForKey } from 'sketch/settings'
 
-export function getSettings () {
+export function getSettings ({
+  keyPrefix = null,
+  shouldRemoveKeyPrefix = true
+} = {}) {
   const defaultSettings = preval.require('./get-default-settings')
   if (!defaultSettings) {
     return {}
   }
-  return Object.keys(defaultSettings).reduce(function (results, key) {
+  const result = {}
+  Object.keys(defaultSettings).forEach(function (key) {
     const sessionValue = sessionVariable(key)
     const savedValue = settingForKey(key)
     const value =
       typeof sessionValue !== 'undefined' ? sessionValue : savedValue
     const defaultValue = defaultSettings[key]
-    results[key] = typeof value !== 'undefined' ? value : defaultValue
-    return results
-  }, {})
+    result[key] = typeof value !== 'undefined' ? value : defaultValue
+  })
+  /* eslint-disable indent */
+  return keyPrefix
+    ? filterSettingsByKeyPrefix({
+        settings: result,
+        keyPrefix: `${keyPrefix}.`,
+        shouldRemoveKeyPrefix
+      })
+    : result
+  /* eslint-enable indent */
+}
+
+function filterSettingsByKeyPrefix ({
+  settings,
+  keyPrefix,
+  shouldRemoveKeyPrefix
+}) {
+  const keyPrefixLength = keyPrefix.length
+  const result = {}
+  Object.keys(settings).forEach(function (key) {
+    if (key.indexOf(keyPrefix) === -1) {
+      return
+    }
+    result[shouldRemoveKeyPrefix ? key.substring(keyPrefixLength) : key] =
+      settings[key]
+  })
+  return result
 }
