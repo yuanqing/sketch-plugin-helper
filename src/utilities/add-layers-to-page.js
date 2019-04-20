23375bfe4bf6bc77dcc4c43964793d4b3344d3cb
yuanqing <desk@yuanqing.sg>
2019-04-20 19:06:56 +0800

    Add `addLayersToPage`

--- src/utilities/add-layers-to-page.js
+++ src/utilities/add-layers-to-page.js
@@ -1,5 +1,9 @@
 import { getCurrentDocument } from './get-document'
 
+export function addLayersToPage ({ layers, page }) {
+  return page.sketchObject.addLayers(layers)
+}
+
 export function addLayersToCurrentPage (layers) {
   const document = getCurrentDocument()
   return document.selectedPage.sketchObject.addLayers(layers)
