2bba9efa35929a35714eeb81f6be833980f4ccfa
yuanqing <desk@yuanqing.sg>
2019-05-12 21:37:30 +0800

    Lint

--- src/common/lint/lint.js
+++ src/common/lint/lint.js
@@ -4,12 +4,7 @@ import { join, resolve } from 'path'
 import { sourceDirectory } from '../constants'
 
 export async function lint () {
-  const eslintConfigPath = resolve(
-    __dirname,
-    '..',
-    '..',
-    '.eslintrc.json'
-  )
+  const eslintConfigPath = resolve(__dirname, '..', '..', '.eslintrc.json')
   const sourceFilesGlob = join(sourceDirectory, '/**/*.js')
   return execa('eslint', [
     '--config',
