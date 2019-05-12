445e3727550b64799e072e1f65c39b6bc55c03e2
yuanqing <desk@yuanqing.sg>
2019-05-12 12:58:52 +0800

    Remove unnecessary files

--- src/settings/get-default-settings.js
+++
@@ -1,9 +0,0 @@
-const { join } = require('path')
-
-const { packageJsonConfigKey } = require('../common/constants')
-
-module.exports = function getDefaultSettings () {
-  const packageJsonPath = join(process.cwd(), 'package.json')
-  const { defaultSettings } = require(packageJsonPath)[packageJsonConfigKey]
-  return defaultSettings || null
-}
