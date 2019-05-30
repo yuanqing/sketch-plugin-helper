49500671df26ad6136efa7fd963b83d89f6f6939
yuanqing <desk@yuanqing.sg>
2019-05-30 21:50:41 +0800

    Fix key name

--- src/utilities/preval-get-plugin-display-name.js
+++ src/utilities/preval-get-plugin-display-name.js
@@ -5,6 +5,6 @@ const { packageJsonConfigKey } = require('../common/constants')
 module.exports = function getPluginDisplayName () {
   const packageJsonPath = join(process.cwd(), 'package.json')
   const packageJson = require(packageJsonPath)
-  const { displayName } = packageJson[packageJsonConfigKey]
-  return displayName || packageJson.name
+  const { name } = packageJson[packageJsonConfigKey]
+  return name || packageJson.name
 }
